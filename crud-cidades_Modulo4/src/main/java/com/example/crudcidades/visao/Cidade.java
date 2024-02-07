ba33d7d0d2da895ce29fcf532ced20932de42af1
Murillo José de Azevedo <68697692+MurilloAzevedo@users.noreply.github.com>
2024-02-07 19:41:48 -0300

    Continuação do Modulo4

--- crud-cidades_Modulo4/src/main/java/com/example/crudcidades/visao/Cidade.java
+++ crud-cidades_Modulo4/src/main/java/com/example/crudcidades/visao/Cidade.java
@@ -5,12 +5,12 @@ import jakarta.validation.constraints.Size;
 
 public final class Cidade {
 
-    @NotBlank(message = "Nome da cidade deve ser informado")
-    @Size(min = 5, max = 60, message = "O nome da cidade deve ter entre 5 e 60 caracteres")
+    @NotBlank(message = "{app.cidade.blank}")
+    @Size(min = 5, max = 60, message = "{app.cidade.size}")
     private final String nome;
 
-    @NotBlank(message = "Sigla do estado deve ser informado")
-    @Size(min = 2, max = 2, message = "A sigla do estado está limitada a dois caracteres")
+    @NotBlank(message = "{app.estado.blank}")
+    @Size(min = 2, max = 2, message = "{app.estado.size}")
     private final String estado;
 
     public Cidade(final String nome, final String estado){
