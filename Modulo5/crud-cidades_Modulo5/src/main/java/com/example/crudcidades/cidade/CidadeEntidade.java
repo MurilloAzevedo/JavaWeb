7afe3577d7592ed5218bb0c33daf6f8e68a493fe
Murillo José de Azevedo <68697692+MurilloAzevedo@users.noreply.github.com>
2024-02-25 18:41:26 -0300

    Add files via upload

---
+++ Modulo5/crud-cidades_Modulo5/src/main/java/com/example/crudcidades/cidade/CidadeEntidade.java
@@ -0,0 +1,39 @@
+package com.example.crudcidades.cidade;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity (name = "cidade")
+public class CidadeEntidade implements Serializable{
+    
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String estado;
+
+    public Long getId() {
+        return this.id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return this.nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEstado() {
+        return this.estado;
+    }
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    
+
+}
